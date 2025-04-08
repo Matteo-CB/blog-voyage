924fbaea1b23622424348e58930f9ec2d67d65b7
Matteo chante <matteo.chbiyikli@gmail.com>
2025-04-08 18:48:30 +0200

    layout style

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,22 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Travel Idea ✈️',
-  description: "Découvrez des destinations uniques, des conseils de voyage, des itinéraires détaillés et des astuces pour voyager malin. Que vous soyez un aventurier en quête d'expériences insolites ou un globe-trotter passionné, notre blog vous guide à travers les plus beaux paysages du monde. 🌎🏕️",
-}
+  title: "Travel Idea ✈️",
+  description:
+    "Découvrez des destinations uniques, des conseils de voyage, des itinéraires détaillés et des astuces pour voyager malin. Que vous soyez un aventurier en quête d'expériences insolites ou un globe-trotter passionné, notre blog vous guide à travers les plus beaux paysages du monde. 🌎🏕️",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="container">
+          <div className="wrapper">{children}</div>
+        </div>
+      </body>
     </html>
-  )
+  );
 }
