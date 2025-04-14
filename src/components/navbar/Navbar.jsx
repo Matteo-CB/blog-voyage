834e07b18139e533d882e55cc5ef99158389e962
Matteo chante <matteo.chbiyikli@gmail.com>
2025-04-14 10:57:57 +0200

    update navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
-import styles from './navbar.module.css'
+import React from "react";
+import styles from "./navbar.module.css";
+import Image from "next/image";
+import Link from "next/link";
 const Navbar = () => {
   return (
     <div className={styles.container}>
-      
+      <div className={styles.social}>
+        <Image src="/facebook.svg" alt="facebook logo" width={24} height={24} />
+        <Image
+          src="/instagram.svg"
+          alt="instagram logo"
+          width={24}
+          height={24}
+        />
+        <Image
+          src="/pinterest.svg"
+          alt="pinterest logo"
+          width={24}
+          height={24}
+        />
+      </div>
+      <div className={styles.logo}>travel idea</div>
+      <div className={styles.links}>
+        <Link href="/">Accueil</Link>
+        <Link href="/">Contact</Link>
+        <Link href="/">À propos</Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
